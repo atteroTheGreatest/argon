2d991b133f1db155fb9dc0718540cfa9cd93fc21
Justyna Ilczuk <justyna.ilczuk@gmail.com>
2014-10-08 11:44:34 +0200

    Add generation of momentums

--- argon_coordinates.py
+++ argon_coordinates.py
@@ -1,20 +1,17 @@
 from __future__ import division
-from math import sqrt
+from math import sqrt, log
+from random import random, choice
 
-a = 0.23  # nm
-b1 = (a, 0, 0)
-b2 = (a/2, a * sqrt(3)/ 2, 0)
-b3 = (a/2, a * sqrt(3)/ 6, a * sqrt(2/3))
+
+k = 0.00831
 
 
 def multiply_vector_by_number(number, vector):
     return [number * x for x in vector]
 
+
 def add_vectors(a, b, c):
     return [x1 + x2 + x3 for x1, x2, x3 in zip(a, b, c)]
-
-assert(multiply_vector_by_number(3, (2, 3, 0)) == [6, 9, 0])
-assert(add_vectors((1, 2, 3), (3, 4, 5), (3, 3, 3)) == [7, 9, 11])
 
 
 def atoms_coordinates(n, b1, b2, b3):
@@ -30,17 +27,81 @@ def atoms_coordinates(n, b1, b2, b3):
     return atoms
 
 
-def save_to_file(atoms, filename):
+def save_to_file(coordinates, filename):
     with open(filename, 'w+') as f:
-        for atom in atoms:
-            f.write('%s\t%s\t%s\n' % (atom[0], atom[1], atom[2]))
+        for x, y, z in coordinates:
+            f.write('%s\t%s\t%s\n' % (x, y, z))
 
-p1 = (a, 0, 0)
-p2 = (0, a, 0)
-p3 = (0, 0, a)
 
-atoms = atoms_coordinates(5, b1, b2, b3)
-save_to_file(atoms, 'aus.dat')
+def read_parameters(filename):
+    with open(filename) as f:
+        data = f.read()
 
-atoms = atoms_coordinates(5, p1, p2, p3)
-save_to_file(atoms, 'simple.dat')
+    numbers = data.split()
+    if len(numbers) != 2:
+        print('Wrong %s data' % filename)
+        return None
+    else:
+        a = float(numbers[0])
+        n = int(numbers[1])
+    return a, n
+
+
+def start_momentum(atoms, m, To):
+    max_energy = - k / 2 * To
+    energies = []
+    for atom in atoms:
+        energies.append((max_energy * log(random()), max_energy * log(random()), max_energy * log(random())))
+
+    # normalization
+    average_energy = sum([element for tupl in energies for element in tupl]) / len(energies) / 3
+
+    norm_factor = - max_energy / average_energy
+    energies = [multiply_vector_by_number(norm_factor, energy) for energy in energies]
+
+    momentums = []
+    
+    def compute_momentum(x):
+        return choice([-1, 1]) * sqrt(2 * m * x)
+
+    for energy in energies:
+        momentum = (compute_momentum(energy[0]), compute_momentum(energy[1]), compute_momentum(energy[2]))
+        momentums.append(momentum)
+    return momentums
+
+
+def main():
+    assert(multiply_vector_by_number(3, (2, 3, 0)) == [6, 9, 0])
+    assert(add_vectors((1, 2, 3), (3, 4, 5), (3, 3, 3)) == [7, 9, 11])
+    
+    parameters = read_parameters('argon.input')
+
+    if not parameters:
+        return 1
+    else:
+        a, n = parameters
+ 
+    # argon structure
+    b1 = (a, 0, 0)
+    b2 = (a/2, a * sqrt(3)/ 2, 0)
+    b3 = (a/2, a * sqrt(3)/ 6, a * sqrt(2/3))
+
+    # box structure
+    p1 = (a, 0, 0)
+    p2 = (0, a, 0)
+    p3 = (0, 0, a)
+
+    atoms = atoms_coordinates(n, b1, b2, b3)
+    save_to_file(atoms, 'aus.dat')
+
+    # atoms = atoms_coordinates(n, p1, p2, p3)
+    # save_to_file(atoms, 'simple.dat')
+    
+    m = 39.948
+    To = 100
+    momentums = start_momentum(atoms, m, To)
+    save_to_file(momentums, 'momentums.dat')
+
+
+if __name__ == '__main__':
+    main()
